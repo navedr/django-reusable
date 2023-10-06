f7ad70dee37ced81f801412869ccf29438691ef8
Naved Rangwala <narangwa@microsoft.com>
2023-10-06 15:25:34 -0700

    changed AppPath

--- django_reusable/config/config_ready_utils.py
+++ django_reusable/config/config_ready_utils.py
@@ -12,12 +12,15 @@ try:
 except ImportError:
     dajaxice_functions = None
 
-AUTO_GENERATED_COMMENT = '// This file is auto generated. Please do not edit. Your changes will be overwritten.\n'
+AUTO_GENERATED_COMMENT = '// This file is auto generated. Please do not edit. Your changes will be overwritten.\n\n'
 
 DAJAXICE_STATIC_TYPES = '''
 import { DajaxiceFn } from "dry-ux";
 
 export const modules: DajaxiceModules = {};\n\n'''
+
+PARSE_FUNCTION = ('const parse = (s: any, o: any) => Object.entries(o).reduce((s, [k, v]) => '
+                  's.replace(new RegExp(":" + k, "g"), v), s);\n\n')
 
 
 def generate_app_path_enums():
@@ -26,14 +29,23 @@ def generate_app_path_enums():
         return
     logger = PrintLogger("[django_reusable] generate_app_path_enums")
 
-    def replace_variable(url):
+    def get_path(url):
         new_url = re.sub(r"%\((\w+)\)s", r":\1", url)
-        return new_url
+        pattern = r"%\((\w+)\)s"
+        variables = re.findall(pattern, url)
+        if variables:
+            return (f'{{\n{spaces(8)}'
+                    f'path: "{new_url}",\n{spaces(8)}'
+                    f'get: ({": any, ".join(variables)}: any): string => '
+                    f'parse("{new_url}", {{ {", ".join(variables)} }})'
+                    f'\n{spaces(4)}}}')
+        return f'"{new_url}"'
 
     from django_reusable.urls.utils import get_all_urls
-    app_urls = ',\n'.join([f'{spaces(4)}"{k}" = "{replace_variable(v)}"'
+    app_urls = ',\n'.join([f'{spaces(4)}"{k}": {get_path(v)}'
                            for (k, v) in sorted(get_all_urls().items(), key=lambda x: x[0])])
-    content = AUTO_GENERATED_COMMENT + 'export enum AppPath {\n' + app_urls + ',\n}\n'
+
+    content = AUTO_GENERATED_COMMENT + PARSE_FUNCTION + 'export const AppPath = {\n' + app_urls + ',\n}\n'
     generate_file_if_updated('app paths', file_path, content, logger)
 
 
