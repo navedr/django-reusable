e3744a9efad3105c86078a1e68ed6ad0adad133d
Naved Rangwala <narangwa@microsoft.com>
2024-07-08 09:48:54 -0700

    added echo message

--- wkhtmltopdf/wkhtmltopdfdocker.sh
+++ wkhtmltopdf/wkhtmltopdfdocker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+echo "Generating PDF using wkhtmltopdf docker image" >> /dev/stderr
 content=""
 while IFS= read -r line; do
    content+="$line"
 done
-echo "$content" | docker run --rm -i -v /Users/narangwa/www:/Users/narangwa/www -v /Users/narangwa/.virtualenvs:/Users/narangwa/.virtualenvs navedrangwala/wkhtmltopdf "$@"
+echo "$content" | docker run --rm -i -v /Users/narangwa/www:/Users/narangwa/www -v /Users/narangwa/.virtualenvs:/Users/narangwa/.virtualenvs -v /var/folders:/var/folders navedrangwala/wkhtmltopdf "$@"
